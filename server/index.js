995b9beeaf07907a937e858feffd3a64959fb572
luohao <luohao937@gmail.com>
2020-05-09 15:43:18 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const path = require("path");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const isDev = process.env.NODE_ENV !== "production";
+
+// Port setup
+const PORT = process.env.PORT || 5000;
+
+// Multi-process to utilize all CPU cores.
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
+    );
+  });
+} else {
+  // Create instance of express
+  const app = express();
+  const router = require("./router");
+
+  // DB Setup
+  mongoose.connect("mongodb://localhost:auth/authentication", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  mongoose.set("useCreateIndex", true);
+
+  const connection = mongoose.connection;
+
+  connection.on("connected", function () {
+    console.log("connected to db");
+  });
+
+  // App setup
+  app.use(morgan("combined"));
+  app.use(bodyParser.json());
+  router(app);
+
+  // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
+
+  // Answer API requests.
+  app.get("/api", function (req, res) {
+    res.set("Content-Type", "application/json");
+    res.send('{"message":"Hello from the custom server!"}');
+  });
+
+  // All remaining requests return the React app, so it can handle routing.
+  app.get("*", function (request, response) {
+    response.sendFile(
+      path.resolve(__dirname, "../react-ui/build", "index.html")
+    );
+  });
+
+  app.listen(PORT, function () {
+    console.error(
+      `Node ${
+        isDev ? "dev server" : "cluster worker " + process.pid
+      }: listening on port ${PORT}`
+    );
+  });
+
+  console.log("Server listening on:", PORT);
+}
